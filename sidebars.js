19cb6c3ab88171da28293b781f46e13a0b3df548
Unknown <dexterslabor@gmail.com>
2020-04-27 11:25:50 +0200

    fixed notarization workshop entries

--- sidebars.js
+++ sidebars.js
@@ -12,8 +12,10 @@ module.exports = {
             'learn/workshops/notarization/intro',
             'learn/workshops/notarization/need-to-notarize',
             'learn/workshops/notarization/opportunities-in-a-digital-age',
-            'learn/workshops/notarization/notarization-powered-by-blockchain',
             'learn/workshops/notarization/renting-an-old-factory',
+            'learn/workshops/notarization/notarization-powered-by-blockchain',
+            'learn/workshops/notarization/comparing-old-new',
+            
           ]
         }
       ]
